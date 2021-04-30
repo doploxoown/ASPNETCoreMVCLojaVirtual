90284da490ae3d56241f220e242f5936e5fe386f
Thiago Figueira Mininel <thi.figueira@hotmail.com>
2021-04-30 12:48:24 -0300

    Limpeza do projeto padrão e adição dos recursos iniciais do projeto.
    Criação de Models/Controllers/Views.
    Validação com DataAnottations.
    Configuração de SMTP para envio de email.
    Criação do banco de dados utilizando EntityFramework e Migrations.

---
+++ Projeto/LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LojaVirtual.Libraries.Email;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contato()
+        {
+            return View();
+        }
+        public IActionResult ContatoAcao()
+        {
+            try
+            {
+                Contato pContato = new Contato
+                {
+                    Nome = HttpContext.Request.Form["nome"],
+                    Email = HttpContext.Request.Form["email"],
+                    Texto = HttpContext.Request.Form["texto"]
+                };
+
+                var listaMensagens = new List<ValidationResult>();
+                var contexto = new ValidationContext(pContato);
+                bool isValid = Validator.TryValidateObject(pContato, contexto, listaMensagens, true);
+
+                if (isValid)
+                {
+                    ContatoEmail.EnviarContatoPorEmail(pContato);
+
+                    ViewData["MSG_S"] = "Mensagem de contato enviado com sucesso!";
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var texto in listaMensagens)
+                    {
+                        sb.Append(texto.ErrorMessage + "<br />");
+                    }
+
+                    ViewData["MSG_E"] = sb.ToString();
+                    ViewData["CONTATO"] = pContato;
+                }
+
+            }
+            catch (Exception e)
+            {
+                ViewData["MSG_E"] = "Ops! Tivemos um erro, tente novamente mais tarde!";
+
+                //TODO - Implementar Log
+            }
+
+
+
+
+            return View("Contato");
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projeto/LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            Produto pProduto = GetProduto();
+
+            return View(pProduto);
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Playstation V",
+                Descricao = "Game Play Avançada",
+                Valor = 3000.00M
+            };
+        }
+    }
+}
